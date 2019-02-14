3e1880186a1e069e68324b6294f9d9f54d6b2ce4
Sean <seanfkh@hotmail.com>
2019-02-14 15:34:02 -0800

    Replaced 'Animator' component with custom 'SpriteAnimator' script.

---
+++ Assets/Scripts/SpriteAnimator.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+
+public class SpriteAnimator : MonoBehaviour
+{
+    [SerializeField]
+    private Sprite[] _frames;
+
+    private SpriteRenderer _sprite;
+
+    [SerializeField]
+    private float _delay, _rate;
+
+    private int _currentFrame;
+
+    private void Start()
+    {
+        _sprite = GetComponent<SpriteRenderer>();
+        InvokeRepeating("Animate", _delay, _rate);
+    }
+
+    private void Animate()
+    {
+        _sprite.sprite = _frames[_currentFrame++ % _frames.Length];
+    }
+}
